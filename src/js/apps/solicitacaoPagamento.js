fc923e8d991bb33486f74944973aaf93a0eb3f8d
Antonio Juevan <86933408+Juevan@users.noreply.github.com>
2025-04-07 16:45:52 -0300

    Update solicitacaoPagamento.js

--- src/js/apps/solicitacaoPagamento.js
+++ src/js/apps/solicitacaoPagamento.js
@@ -1,132 +1,131 @@
+$(document).ready(function () {
+  $('#inpaprovadorDeNatureza').val('antonio.silva@raizeducacao.com.br');
+  $("b:contains('hide')").closest('tr').hide();
 
-$(document).ready(function() {
-    var $inputDataVencimento = $('#inpdataDeVencimento');
-    var $inputDiasPagamento = $('#inpdiasParaOPagamento');
+  verificarQtdLinhas();
 
-    if (!$inputDataVencimento.length) {
-        console.error("Elemento inpdataDeVencimento não encontrado.");
-        return;
-    }
-    if (!$inputDiasPagamento.length) {
-        console.error("Elemento inpdiasParaOPagamento não encontrado.");
-        return;
-    }
+  $(document).on('click', '#btnInsertNewRow', function () {
+    setTimeout(verificarQtdLinhas, 10);
+  });
 
-    $inputDataVencimento.on('change', function() {
-        var dataString = $inputDataVencimento.val();
+  $(document).on('click', '.btn-delete-mv', function () {
+    setTimeout(verificarQtdLinhas, 10);
+  });
 
-        if (dataString) {
-            var partesData = dataString.split("/");
 
-            if (partesData.length !== 3) {
-                console.error("Formato de data inválido. Use dd/mm/aaaa.");
-                return;
-            }
+  $(document).on('change', '#inpcondicaoDePagamento', function () {
+    setTimeout(verificarQtdLinhas, 10);
+  });
 
-            var dia = parseInt(partesData[0], 10);
-            var mes = parseInt(partesData[1], 10) - 1;
-            var ano = parseInt(partesData[2], 10);
-
-            var dataDeVencimento = new Date(ano, mes, dia);
-            var dataAtual = new Date();
-
-            dataAtual.setHours(0, 0, 0, 0);
-            dataDeVencimento.setHours(0, 0, 0, 0);
-
-            var diferencaEmDias = Math.ceil(
-                (dataDeVencimento - dataAtual) / (1000 * 60 * 60 * 24)
-            );
-
-            $inputDiasPagamento.val(diferencaEmDias);
-            console.log(`Faltam ${diferencaEmDias} dias para o vencimento.`);
-        } else {
-            console.error("Campo de data de vencimento está vazio.");
-        }
+  $(document).on('change', '#inptipoDaSolicitacao', function () {
+    defineCodigoDoMovimento()
+  });
+  
+  $('#inptipoDaSolicitacao, #inptipoDoPagamento, #inpcontasDeConsumo, #inptipoDeItem, #inpoutrosGastos, #inpcentroDeCusto, #inpnaturezaOrcamentaria, #inpfornecedor').on('change', function() {
+    defineCodigoDoMovimento()
+	})
+  
+    $('#inpdataDeVencimento').on('change', function() {
+        validaDataVencimento(this);
     });
+
 });
+
+function defineCodigoDoMovimento() {
+  const tipoSolicitacao = $('#inptipoDaSolicitacao').val()
+  const tipoDoPagamento = $('#inptipoDoPagamento').val()
+
+  switch (tipoSolicitacao) {
+    case "AD":
+      $("#inpcodigoDoMovimento").val("1.2.06")
+      $("#inpserie").val("AD")
+      break;
+    case "PG":
+      if (tipoDoPagamento == "CC") {
+        let contasDeConsumo = $("#inpcontasDeConsumo").val();
+        $("#inpserie").val("1")
+        if(contasDeConsumo == "A"){
+          $("#inpcodigoDoMovimento").val("1.2.09")
+        } else if (contasDeConsumo == "E") {
+          $("#inpcodigoDoMovimento").val("1.2.10")
+        } else if (contasDeConsumo == "T") {
+          $("#inpcodigoDoMovimento").val("1.2.11")
+        } else if (contasDeConsumo == "G") {
+          $("#inpcodigoDoMovimento").val("1.2.12")
+        }
+      
+      } else if (tipoDoPagamento == "NF") {
+        if ($("#inptipoDeItem").val() == "Material") {
+          $("#inpserie").val($("#inpserieDaNF").val())
+          $("#inpcodigoDoMovimento").val("1.2.01")
+        } else if ($("#inptipoDeItem").val() == "Serviço") {
+          $("#inpserie").val($("#inpserieDaNF").val())
+          $("#inpcodigoDoMovimento").val("1.2.03")
+        }
+      } else if (tipoDoPagamento == "OG") {
+        if ($("#inpoutrosGastos").val() == "RF") {
+          $("#inpserie").val("1")
+          $("#inpcodigoDoMovimento").val("1.2.16")
+        } else if ($("#inpoutrosGastos").val() == "AL" && $("#inptipoDoLocador").val() == "Pessoa Jurídica") {
+          $("#inpserie").val("ALPJ")
+          $("#inpcodigoDoMovimento").val("1.2.17")
+        } else if ($("#inpoutrosGastos").val() == "AL" && $("#inptipoDoLocador").val() == "Pessoa Fisica") {
+          $("#inpserie").val("ALPF")
+          $("#inpcodigoDoMovimento").val("1.2.08")
+        }
+      } else if (tipoDoPagamento == "TM") {
+        $("#inpserie").val($("#inpserieDaNF").val())
+        $("#inpcodigoDoMovimento").val("1.2.25")
+      }
+      break;
+    case "RE":
+      $("#inpcodigoDoMovimento").val("1.2.07")
+      $("#inpserie").val("PR")
+      break;
+    case "FF":
+      $("#inpcodigoDoMovimento").val("1.2.29")
+      $("#inpserie").val("PR")
+      break;
+  }
+}
+
+function verificarQtdLinhas() {
+  const tabelaAlvo = $('input#inpnumeroDaParcela').closest('table');
+  const qtdLinhas = tabelaAlvo.find('tbody tr').length - 1;
+  const qtdParcelas = $("#inpqtdParcelas").val();
+  const codigoDaCondicao = $("#inpcodigoDaCondicaoPagamento").val();
+  if (qtdParcelas != "" && codigoDaCondicao != "") {
+    tabelaAlvo.find('#btnInsertNewRow').prop('disabled', qtdLinhas >= qtdParcelas);
+  }
+}
+
 function validaDataVencimento(input) {
     let dataInput = input.value;
-    let [dia, mes, ano] = dataInput.split('/');
-    let dataEscolhida = new Date(ano, mes - 1, dia);
+    let dataEscolhida = converteParaDate(dataInput);
 
     let dataAtual = new Date();
     dataAtual.setHours(0, 0, 0, 0);
 
     if (dataEscolhida <= dataAtual) {
-        cryo_alert('<p style="color: red; text-align: center;">A data escolhida não pode ser a data atual ou uma data anterior.</p>');
+        alert('A data escolhida não pode ser a data atual ou uma data anterior.');
         input.value = '';
         $('#inptipoDePedido').hide();
         return;
     }
 
-    while (isFeriadoOuFimDeSemana(dataEscolhida)) {
-        dataEscolhida.setDate(dataEscolhida.getDate() + 1);
-    }
+    dataEscolhida = ajustarParaDiaPermitido(dataEscolhida);
 
     const diferencaTempo = dataEscolhida - dataAtual;
     const diferencaDias = diferencaTempo / (1000 * 3600 * 24);
 
-    if (diferencaDias <= 7) {
+    if (diferencaDias >= 3 && diferencaDias < 5  ) {
         $('#inptipoDePedido').val('EMERGÊNCIAL');
-    } else {
+    } else if (diferencaDias >= 5) {
         $('#inptipoDePedido').val('REGULAR');
+    } else {
+      alert('A data escolhida não pode ser utilizada.');
     }
 
     $('#inptipoDePedido').show();
-
     input.value = formatarData(dataEscolhida);
-}
-
-function vencimentoParcela(input) {
-    let dataInput = input.value;
-    let [dia, mes, ano] = dataInput.split('/');
-    let dataEscolhida = new Date(ano, mes - 1, dia);
-
-    let dataAtual = new Date();
-    dataAtual.setHours(0, 0, 0, 0);
-
-    if (dataEscolhida <= dataAtual) {
-        cryo_alert('<p style="color: red; text-align: center;">A data escolhida não pode ser a data atual ou uma data anterior.</p>');
-        input.value = '';
-        $('#inptipoDePedido').hide();
-        return;
-    } else {
-        if (isFeriadoOuFimDeSemana(dataEscolhida)) {
-            let dataFinal = adicionarDiasUteis(dataEscolhida, 1);
-            input.value = formatarData(dataFinal);
-        }
-
-        var datasDeVencimento = [];
-
-        $('input[data-name="vencimentoDaParcela"]').each(function () {
-            var valorData = $(this).val().trim();
-
-            if (valorData !== '') {
-                var partesData = valorData.split('/');
-                var dataISO = new Date(partesData[2], partesData[1] - 1, partesData[0]);
-
-                datasDeVencimento.push(dataISO);
-            }
-        });
-
-        if (datasDeVencimento.length > 0) {
-            var menorData = new Date(Math.min.apply(null, datasDeVencimento));
-
-            const diferencaTempo = menorData - dataAtual;
-            const diferencaDias = diferencaTempo / (1000 * 3600 * 24);
-
-            if (diferencaDias <= 2) {
-                $('#inptipoDePedido').val('EMERGÊNCIAL');
-            } else if (diferencaDias <= 4 && diferencaDias > 2) {
-                $('#inptipoDePedido').val('URGENTE');
-            } else {
-                $('#inptipoDePedido').val('REGULAR');
-            }
-
-            $('#inptipoDePedido').show();
-            $('#inpdataDeVencimento').val(formatarData(menorData));
-        } else {
-            console.log('Nenhuma data de vencimento encontrada.');
-        }
-    }
 }
