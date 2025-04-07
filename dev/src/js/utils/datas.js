6ce835fb5c734f17a75bdcad5e6e33c4f06d9c95
Juevan <86933408+Juevan@users.noreply.github.com>
2025-04-07 18:57:31 +0000

    Deploying to gh-pages from @ RaizEducacaoDev/Ticket-Raiz@220a33f0896e75d96f1e6e9447fb1075d3f0541b 🚀

--- dev/src/js/utils/datas.js
+++ dev/src/js/utils/datas.js
@@ -11,15 +11,35 @@ function obterDataFormatadaMesDia(data) {
     return `${mes}-${dia}`;
 }
 
-function isFeriadoOuFimDeSemana(data) {
-    const feriados = ['01-01', '04-21', '05-01', '09-07', '10-12', '11-02', '11-15', '12-25'];
-    const diaSemana = data.getDay();
+function isFeriado(data) {
+    const feriados = [
+        '01-01', // Confraternização Universal
+        '01-20', // Dia de São Sebastião (Municipal)
+        '03-04', // Carnaval (Estadual)
+        '04-18', // Sexta-Feira Santa
+        '04-21', // Tiradentes
+        '04-23', // Dia de São Jorge (Estadual)
+        '05-01', // Dia do Trabalho
+        '06-19', // Corpus Christi
+        '09-07', // Independência do Brasil
+        '10-12', // Nossa Senhora Aparecida
+        '11-02', // Finados
+        '11-15', // Proclamação da República
+        '11-20', // Dia da Consciência Negra (Estadual)
+        '12-25'  // Natal
+    ];
     const dataFormatadaMesDia = obterDataFormatadaMesDia(data);
+    return feriados.includes(dataFormatadaMesDia);
+}
 
-    const isFimDeSemana = (diaSemana === 0 || diaSemana === 6);
-    const isFeriado = feriados.includes(dataFormatadaMesDia);
+function isFimDeSemana(data) {
+    const diaSemana = data.getDay();
+    return diaSemana === 0 || diaSemana === 6;
+}
 
-    return isFimDeSemana || isFeriado;
+function isDiaPermitido(data) {
+    const diaSemana = data.getDay();
+    return diaSemana === 1 || diaSemana === 3 || diaSemana === 5;
 }
 
 function adicionarDiasUteis(data, diasUteis) {
